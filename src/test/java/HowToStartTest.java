ace7e8b1608786dceb9483959c8702518a271510
vladimirpekarski <vladimir.pekarski@gmail.com>
2015-02-19 23:54:50 +0300

    just checking

--- src/test/java/HowToStartTest.java
+++ src/test/java/HowToStartTest.java
@@ -16,6 +16,14 @@ public class HowToStartTest extends TestBase {
     }
 
     @Test
+    public void loginTest2() {
+        open("", HomePage.class)
+                .goTo("Form Authentication", LoginPage.class)
+                .login("tomsmith", "SuperSecretPassword!")
+                .shouldLogin();
+    }
+
+    @Test
     public void negativeLoginTest() {
         open("", HomePage.class)
                 .goTo("Form Authentication", LoginPage.class)
